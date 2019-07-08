45e16ead95003b62975685f6068a83233faebe7e
Binyan Lin <obinyans@gmail.com>
2019-07-08 14:48:43 -0700

    basic handlebars html draft

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 // Dependencies
 //----------------------------------------------------
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
 // Middleware
 //----------------------------------------------------
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public')); 
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 
 // Routing
 //----------------------------------------------------
-app.use(routes);
+// app.use(routes);
 
-// Listen to the server
-//----------------------------------------------------
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Routes
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/dashboard", function(req, res) {
+  res.render("dashboard");
+});
+
+app.get("/search", function(req, res) {
+  res.render("search");
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
