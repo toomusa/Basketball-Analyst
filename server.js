927739af84d6b80d297cf5317f789dcc972a5bfc
Ed Chebli <chah762002@yahoo.fr>
2019-07-08 11:55:12 -0700

    added handlebars file

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+// Dependencies
+//----------------------------------------------------
 const express = require("express");
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 const routes = require("./routes");
 
+// Middleware
+//----------------------------------------------------
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
+// Routing
+//----------------------------------------------------
 app.use(routes);
 
+// Listen to the server
+//----------------------------------------------------
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
