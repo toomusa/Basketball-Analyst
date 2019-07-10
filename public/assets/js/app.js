70d4f3966e32863d0eb284baace81971c8cc87c3
Binyan Lin <obinyans@gmail.com>
2019-07-10 11:00:27 -0700

    created checkbox dynamic maker in app.js

---
+++ public/assets/js/app.js
@@ -0,0 +1,11 @@
+const checkboxMaker = (obj) => {
+  for(let key in obj) {
+    let newDiv = $(`<div class="col-sm-4">`);
+    newDiv.append(`<div class="form-check">
+    <input class="form-check-input" type="checkbox" id="${key}" value="${key}">
+    <label class="form-check-label" for="inlineCheckbox3">${key}</label>
+    </div>`);
+    $('.searchChecks').prepend(newDiv);
+  }
+}
+
