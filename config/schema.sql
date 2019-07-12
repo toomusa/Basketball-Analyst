a26e546f264491861622de91dc07664b2c2fe54b
toomusa <3895499+toomusa@users.noreply.github.com>
2019-07-11 22:37:34 -0700

    added user table functions

--- config/schema.sql
+++ config/schema.sql
@@ -4,11 +4,18 @@ CREATE DATABASE bball_db;
 
 USE bball_db;
 
+DROP TABLE IF EXISTS ????
+
 CREATE TABLE players (
     id INTEGER NOT NULL AUTO_INCREMENT,
-    playerName VARCHAR(100) NOT NULL,
+    playerId INTEGER (10) NOT NULL,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
     teamName VARCHAR(100) NOT NULL,
-    teamId INTEGER,
+    teamId INTEGER (10) NOT NULL,
     PRIMARY KEY (id)
 );
 
+-- JSON data coming in
+-- translate to:
+-- INSERT INTO players (playerId, firstName, lastName, teamName, teamId)
--- controllers/api/apiController.js
+++ controllers/api/apiController.js
@@ -11,28 +11,59 @@ msf.authenticate("1895dd8d-e910-476e-a495-9f7980", "MYSPORTSFEEDS");
 module.exports = {
     queryData: async (req, res) => {
         // let {season, position, roster, playerString, teamString, endpoint} = req.body;
+        let {checkBox} = req.body;
+        console.log(checkBox)
+
+        // splice out first 4 characters, determine which endpoint to query, and set endpoint
+        let objKeys = Object.keys(checkBox);        
+        let prefixes = objKeys.map(item => {
+            let elem = item.slice(0,4);
+            return elem;
+        })
+        let prefix = [...new Set(prefixes)].toString();
+
+        let endpoint;
+        if (prefix === "plyr") {
+            endpoint = "players";
+        } else if (prefix === "plij") {
+            endpoint = "playerInjuries";
+        } else if (prefix === "dpgl") {
+            endpoint = "dailyPlayerGameLogs";
+        } else if (prefix === "dtgl") {
+            endpoint = "dailyTeamGameLogs";
+        } else if (prefix === "spls") {
+            endpoint = "seasonalPlayerStats";
+        } else if (prefix === "stms") {
+            endpoint = "seasonalTeamStats";
+        } else {
+            console.log("Something went wrong with prefix");
+        }
+
         let season = "2016-2017-regular";
-        let endpoint = "players";
+        // let endpoint = "players";
         let playerString = "stephen-curry";
         let teamString = "gsw";
         let roster = "assigned-to-roster";
         let position = "pg";
-        // let queryUrl = `"nba", "${season}", "${endpoint}", "json", {player: "${playerString}", team: "${teamString}", 
-        //                 rosterstatus: "${roster}", position: "${position}", force: true}`;
-        // let queryUrl2 = `"nba", ${season}, ${endpoint}, "json", {player: ${playerString}, team: ${teamString}, 
-        //                 rosterstatus: ${roster}, position: ${position}, force: true}`;
-        // console.log(queryUrl);
-        // console.log(queryUrl2);
-
-        let obj = {
-            "key lock": 4,
-            "item 4": "people"
-        }
-        console.log(obj["item 4"])
         var data = await msf.getData('nba', season, endpoint, 'json', {player: playerString, team: teamString, 
                                         rosterstatus: roster, position: position, force: true});
-        
         let playersData = await apiModel.players(data);
         console.log(playersData);
+
+        const userTableQuery = () => {
+            let userTable = {};
+            return new Promise ((resolve, reject) => {
+                for(let key in playersData) {
+                    for (let item in checkBox) {
+                        if (key === item) {
+                            userTable[checkBox[item]] = playersData[key];
+                        }
+                    }
+                }
+                console.log(userTable)
+                resolve();
+            })
+        }
+        userTableQuery();
     }
 };
